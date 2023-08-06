46869427db52b362380b9014e4425bb3093af77e
PearlClone <benjaminlecomte37@gmail.com>
2023-08-06 21:53:12 +0200

    update

---
+++ static/footer/footer.js
@@ -0,0 +1,20 @@
+const homeFooter = document.querySelector('#homeFooter');
+const aboutFooter = document.querySelector('#aboutFooter');
+const projectsFooter = document.querySelector('#projectsFooter');
+const archivesFooter = document.querySelector('#archivesFooter');
+const contactsFooter = document.querySelector('#contactsFooter');
+
+// Récupérer le nom de la page
+const currentPage = window.location.pathname.split("/").pop().split(".")[0];
+
+console.log(currentPage)
+
+// Vérifier si le nom de la currentPage suivi du mot "Footer" correspond à une des constantes
+const footerConstants = ["home", "about", "projects", "archives", "contacts"];
+const footerMenuItems = [homeFooter, aboutFooter, projectsFooter, archivesFooter, contactsFooter];
+for (let i = 0; i < footerConstants.length; i++) {
+  if (currentPage === footerConstants[i]) {
+    footerMenuItems[i].classList.add("active");
+    break;
+  }
+}
--- templates/home/home.php
+++ templates/home/home.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/portfolio/static/header/header.css">
+    <link rel="stylesheet" href="/portfolio/static/footer/footer.css">
     <link rel="stylesheet" href="./home.css">
     <title>Home</title>
 </head>
@@ -47,13 +48,106 @@
                 </div>
             </div>    
         </div>
-        <div class="categories">
 
+        <div class="categories">
+            <div class="listCat">
+                <div class="listCatLeft">
+                    <p>PROGRAMMER</p>
+                    <p>STUDENT</p>
+                    <p>DESIGNER</p>
+                </div>
+                <div class="listCatRight">
+                    <p>2019</p>
+                    <div class="ligne"></div>
+                    <p>2023</p>
+                </div>
+            </div>
         </div>
-         
+
+        <div class="tuiles">
+
+            <div class="projectsBanner">
+                <div class="projectsBannerContent">
+                    <img class="button" src="/portfolio/templates/home/assets/buttonProjects.png" alt="">
+                    <div class="projectsBannerContentText">
+                    <p>All my projects <br> since high <br> school</p>
+                    <img src="/portfolio/templates/home/assets/imageProjects.png" alt=""> 
+                    </div>  
+                </div>                
+            </div>
+
+            <div class="dualTuilesFlex">
+                <div class="dualTuiles">
+
+                    <div class="recent">
+                        <div class="top">
+                            <p>RECENT</p>
+                            <img class="button" src="/portfolio/templates/home/assets/recentTop.png" alt="">
+                        </div>
+                        <img class="banner" src="/portfolio/templates/home/assets/middleRecent.png" alt="">
+                        <div class="bottom">
+                            <div class="date">
+                                <p>SnapOCR</p>
+                                <p class="dateText">2023</p>
+                            </div>
+                            <p>Web application for extracting texte from image thanks to js tensorflow training and save it</p>
+                        </div>
+                    </div>
+
+                    <div class="skils">
+                        <p class="title">Weapons of Choice</p>
+                        <div class="settings">
+                            <img class="circle" src="/portfolio/templates/home/assets/settings.png" alt="">
+                            <div class="settingsText">
+                                <p>This is my favorite tools set<span>.</span>  I mainly work for web application and make video games also<span>.</span></p>
+                                <img class="skilsImage" src="/portfolio/templates/home/assets/skils.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>    
+            </div>
+
+            <div class="contactFlex">
+                <div class="contact">
+                    <div class="contactSize">
+                       <div class="link">
+                        <div class="linkTop">
+                            <img src="/portfolio/templates/home/assets/github.png" alt="">
+                            <img src="/portfolio/templates/home/assets/insta.png" alt="">
+                            <img src="/portfolio/templates/home/assets/mail.png" alt="">
+                        </div>
+                        <div class="linkBottom">
+                            <img src="/portfolio/templates/home/assets/twitter.png" alt="">
+                            <img src="/portfolio/templates/home/assets/linkedin.png" alt="">
+                            <img src="/portfolio/templates/home/assets/discord.png" alt="">
+                        </div>
+                    </div>
+                    <div class="text">
+                        <p class="title">Contacts & Social Media</p>
+                        <p class="subtitle">Interested by my work? contacted me or follow me on my different social networks to follow my most recent work</p>
+                    </div> 
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div class="archiveFlex">
+                <div class="archives">
+                    <div class="archiveContent">
+                        <p>Archives</p>
+                        <img src="/portfolio/templates/home/assets/buttonArchives.png" alt="">  
+                    </div>
+                </div>
+            </div>
+
+            <div class="space"></div>
+        </div>
+        
+        <?php include '../../static/footer/footer.php'; ?>
     </section>
     
 
 <script src="/portfolio/static/header/header.js"></script>
+<script src="/portfolio/static/footer/footer.js"></script>
 </body>
 </html>
