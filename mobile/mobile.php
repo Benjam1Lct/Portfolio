e3389daf40471950946e908c69825765b3b5991c
PearlClone <benjaminlecomte37@gmail.com>
2023-08-07 23:04:23 +0200

    update

--- mobile/mobile.php
+++ mobile/mobile.php
@@ -4,17 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./mobile.css">
+    <link rel="icon" href="../logo.png" type="image/x-icon">
     <title>Waiting</title>
 </head>
 <body>
-    <p class="title">
-        Mobile version <br> in developpement
-    </p>
-    <div class="pcVersion">
-        <p>Portfolio working on PC</p>
+    <div class="mobile">
+        <p class="title">
+            Mobile version <br> in developpement
+        </p>
+        <div class="pcVersion">
+            <p>Portfolio working on PC</p>
+        </div>
+        <p class="date">
+            winter 2024
+        </p>
     </div>
-    <p class="date">
-        winter 2024
-    </p>
+    
 </body>
 </html>
