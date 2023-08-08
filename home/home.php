137d65b5812afafda8e09038e32573439ac33990
PearlClone <benjaminlecomte37@gmail.com>
2023-08-08 20:26:57 +0200

    update

--- home/home.php
+++ home/home.php
@@ -1,3 +1,9 @@
+<?php
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Pragma: no-cache");
+header("Expires: Wed, 11 Jan 1984 05:00:00 GMT");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
